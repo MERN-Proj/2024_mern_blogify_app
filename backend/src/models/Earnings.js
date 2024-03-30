27b213d2a9d118872d855c2d0ccf06264bec5421
mehedi_hasan <mehedi609@gmail.com>
2024-03-31 02:28:22 +0900

    implemented earnings model

---
+++ backend/src/models/Earnings.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+//schema
+const earningsSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    calculatedOn: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+//model
+const Earnings = mongoose.model('Earnings', earningsSchema);
+export default Earnings;
