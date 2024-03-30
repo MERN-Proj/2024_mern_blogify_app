652818f861001e01ea73a77042d247305d683732
mehedi_hasan <mehedi609@gmail.com>
2024-03-31 02:28:13 +0900

    implemented comment model

---
+++ backend/src/models/Comment.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+//schema
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+//model
+const Comment = mongoose.model('Comment', commentSchema);
+export default Comment;
