f00bced7792243c83b9bd4128721e2efdd699740
mehedi_hasan <mehedi609@gmail.com>
2024-03-31 02:28:04 +0900

    implemented category model

---
+++ backend/src/models/Category.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+//schema
+const categorySchema = new mongoose.Schema(
+  {
+    categoryName: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+//model
+const Category = mongoose.model('Category', categorySchema);
+export default Category;
